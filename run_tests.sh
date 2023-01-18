375b8e2211dd3f53cdf995635d21f13e38bd7085
Ronald Krist <ronald.krist@cesnet.cz>
2023-01-18 10:53:31 +0100

    bug fix 2.1

--- run_tests.sh
+++ run_tests.sh
@@ -10,5 +10,6 @@ fi
 oarepo-compile-model ./tests/$MODEL.yaml --output-directory ./tests/$MODEL --profile model,files -vvv
 python3 -m venv $VENV
 . $VENV/bin/activate
+pip install -U setuptools pip wheel
 pip install "./tests/$MODEL[tests]"
 pytest tests/$MODEL/tests/files
