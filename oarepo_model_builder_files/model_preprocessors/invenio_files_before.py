3da3d4886487d41e7dfe6c409a3f13ca566d141e
Ronald Krist <ronald.krist@cesnet.cz>
2023-01-26 12:22:05 +0100

    refactoring file model preprocessor into before and after, schema and mapping not on record fix

--- oarepo_model_builder_files/model_preprocessors/invenio_files_before.py
+++ oarepo_model_builder_files/model_preprocessors/invenio_files_before.py
@@ -1,8 +1,8 @@
 from oarepo_model_builder.model_preprocessors import ModelPreprocessor
 from oarepo_model_builder.utils.camelcase import camel_case
 
-class InvenioModelFilesPreprocessor(ModelPreprocessor):
-    TYPE = "invenio_files"
+class InvenioModelFilesBeforePreprocessor(ModelPreprocessor):
+    TYPE = "invenio_files_before"
 
     def transform(self, schema, settings):
         files = schema.schema.files
@@ -14,7 +14,6 @@ class InvenioModelFilesPreprocessor(ModelPreprocessor):
         files.setdefault("record-prefix", f"{parent_record_prefix}File")
         files.setdefault("record-permissions-class", model.record_permissions_class)
         files.setdefault("profile-package", "files")
-        files.setdefault("record-search-options-class", "")
-        #files.setdefault("extra-fixtures", ["location"])
+        files.setdefault("extra-fixtures", ["location"])
         files.setdefault("collection-url", f'{model["collection-url"]}<pid_value>')
         model.setdefault("record-service-config-components", []).append("invenio_records_resources.services.records.components.FilesOptionsComponent")
