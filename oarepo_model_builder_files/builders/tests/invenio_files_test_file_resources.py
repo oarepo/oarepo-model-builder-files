f2e4dd2276bfeda1ea665ddc88a025a08873baa7
Ronald Krist <ronald.krist@cesnet.cz>
2023-06-15 15:30:18 +0200

    tests can be disabled

--- oarepo_model_builder_files/builders/tests/invenio_files_test_file_resources.py
+++ oarepo_model_builder_files/builders/tests/invenio_files_test_file_resources.py
@@ -7,7 +7,10 @@ class InvenioFilesTestFileResourcesBuilder(InvenioFilesParentBuilder):
 
     def finish(self, **extra_kwargs):
         tests = getattr(self.current_model, "section_tests")
-        super().finish(fixtures=tests.fixtures, test_constants=tests.constants, **extra_kwargs)
+        files_tests = getattr(self.current_model, "section_files_tests")
+        super().finish(fixtures=tests.fixtures | files_tests.fixtures,
+                       test_constants=tests.constants | files_tests.constants,
+                       **extra_kwargs)
 
     def _get_output_module(self):
         return f'{self.current_model.definition["tests"]["module"]}.files.test_file_resources'
--- oarepo_model_builder_files/datatypes/components/__init__.py
+++ oarepo_model_builder_files/datatypes/components/__init__.py
@@ -1,5 +1,7 @@
 from .file import FileComponent, FileMetadataComponent
 from .files_field import FilesFieldModelComponent
+from .files_tests import FilesModelTestComponent
 from .parent_record import ParentRecordComponent
 
-file_components = [FileComponent, FileMetadataComponent, ParentRecordComponent, FilesFieldModelComponent]
+file_components = [FileComponent, FileMetadataComponent, ParentRecordComponent, FilesFieldModelComponent,
+                   FilesModelTestComponent]
