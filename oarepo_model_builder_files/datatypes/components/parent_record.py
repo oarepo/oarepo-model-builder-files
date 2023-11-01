5552ab0856e15ad254363f8b5aa0bfbc819bf0ac
Mirek Simek <miroslav.simek@cesnet.cz>
2023-11-01 10:55:53 +0100

    Better import for parent record service components (#15)

--- oarepo_model_builder_files/datatypes/components/parent_record.py
+++ oarepo_model_builder_files/datatypes/components/parent_record.py
@@ -38,5 +38,5 @@ class ParentRecordComponent(DataTypeComponent):
             datatype,
             "service-config",
             "components",
-            "invenio_records_resources.services.records.components.FilesOptionsComponent",
+            "{{invenio_records_resources.services.records.components.FilesOptionsComponent}}",
         )
